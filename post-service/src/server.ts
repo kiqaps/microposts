c1586b717f5ba693df1d8f5bac24633915e468f8
Caíque Araújo Spósito <kiqaps@gmail.com>
2020-09-05 16:39:34 -0300

    PostService: rota hello world implementada

---
+++ post-service/src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  return res.json({
+    message: 'Hello PostService!',
+  })
+})
+
+app.listen(3000, () => {
+  console.log(`Server running at port 3000`)
+})
