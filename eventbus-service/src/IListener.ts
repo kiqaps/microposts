2b507c2520180b4d6a4c5b565258defc98ec6325
Caíque Araújo Spósito <kiqaps@gmail.com>
2020-09-08 17:50:42 -0300

    EventBus: implementaçao inicial

---
+++ eventbus-service/src/IListener.ts
@@ -0,0 +1,3 @@
+export interface IListeners {
+  [topic: string]: string[]
+}
---
+++ eventbus-service/src/server.ts
@@ -0,0 +1,69 @@
+import express from 'express'
+import cors from 'cors'
+import axios from 'axios'
+
+import { IListeners } from './IListener'
+import { IEvent } from './IEvent'
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+const listeners: IListeners = {}
+
+app.post('/subscribe', (req, res) => {
+  const { topic, address } = req.body
+
+  if (!topic || !address) {
+    return res.status(400).json({
+      error: 'Missing topic or address',
+    })
+  }
+
+  listeners[topic] = listeners[topic] || []
+  listeners[topic].push(address)
+  return res.status(204).send()
+})
+
+app.post('/unsubscribe', (req, res) => {
+  const { topic, address } = req.body
+
+  if (!topic || !address) {
+    return res.status(400).json({
+      error: 'Missing topic or address',
+    })
+  }
+
+  if (listeners[topic]) {
+    const index = listeners[topic].findIndex(
+      existentAddr => existentAddr === address,
+    )
+    if (index >= 0) {
+      listeners[topic].splice(index, 1)
+    }
+  }
+
+  return res.status(204).send()
+})
+
+app.post('/event', (req, res) => {
+  const { topic, payload } = req.body
+
+  if (!topic) {
+    return res.status(400).json({
+      error: "Missing event's topic",
+    })
+  }
+
+  const event: IEvent = { topic }
+  if (payload) event.payload = payload
+
+  if (listeners[topic]) {
+    for (const address of listeners[topic]) {
+      axios.post(`${address}/event`, event)
+    }
+  }
+  return res.status(204).send()
+})
+
+app.listen(3000)
